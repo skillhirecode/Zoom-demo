0c7328f074a4e351d390a56aa1985d35b3399080
Harish S <harish@wiizb.com>
2024-04-15 21:44:20 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ function App() {
             <Route path="/homePage" element={<HomePage />} />
           </Routes>
           <Routes>
-            <Route path="/Login" element={<Login />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
           
         </Router>
